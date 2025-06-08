64030220a5bb27d16743f4bb61d838f70a6c91c8
juanRCoder <guillecarck@gmail.com>
2025-06-08 13:17:40 -0500

    refactor: refactor product view [TO-03]

--- src/shared/Navbar.tsx
+++ src/shared/Navbar.tsx
@@ -2,11 +2,11 @@ import { Store, ShoppingCart, ClipboardList, User } from 'lucide-react';
 
 export default function Navbar() {
   return (
-    <section className='relative rounded-tl-xl rounded-tr-xl bg-[#29386F] py-3 flex-wrap gap-3 flex justify-evenly pt-5'>
-      <span className="absolute top-0 left-[34px] border-l-12 border-r-14 border-t-12 border-l-transparent border-r-transparent border-t-[#FFC859]" />
+    <section className='fixed bottom-0 left-1/2 -translate-x-1/2 w-full max-w-sm bg-[#208572] py-3 flex-wrap gap-3 flex justify-evenly pt-5 z-50'>
+      <span className="absolute top-0 left-[40px] border-l-12 border-r-14 border-t-12 border-l-transparent border-r-transparent border-t-white" />
       <div className='flex flex-col items-center cursor-pointer'>
-        <Store color='#FFC859' size={32} />
-        <p className='text-[#FFC859] text-sm'>Tienda</p>
+        <Store color='#FFFFFF' size={32} />
+        <p className='text-white text-sm'>Tienda</p>
       </div>
       <div className='flex flex-col items-center cursor-pointer'>
         <ShoppingCart color='#FFFFFF' size={32} />
--- src/views/ProductsView.tsx
+++ src/views/ProductsView.tsx
@@ -1,117 +1,97 @@
-import { Search, SlidersHorizontal, Heart, Plus, ChevronRight } from 'lucide-react';
+import { Search, SlidersHorizontal, Store, ChevronRight, Heart } from 'lucide-react';
 import Navbar from '../shared/Navbar';
 
 export default function ProductsView() {
   return (
-    <section className="max-w-sm m-auto bg-white" style={{ fontFamily: "'Roboto', sans-serif" }}>
-      <h1 className="text-center text-[#29386F] font-bold text-4xl py-5">Mi Tienda</h1>
-      {/* SECCION DE SEARCH */}
-      <section className='flex px-2 gap-4'>
-        <div className='relative flex-1'>
-          <Search color="#29386F" strokeWidth={2.5} className='absolute top-2 left-3' />
-          <input
-            type="search"
-            placeholder="Buscar"
-            className="w-full rounded-xl border border-[#52514F80] bg-transparent px-5 pl-12 py-2 text-[#52514F] placeholder:text-[#52514F] placeholder:text-lg placeholder:font-normal outline-none"
-          />
+    <section className="relative flex flex-col justify-between max-w-sm m-auto min-h-screen bg-white"
+      style={{ fontFamily: "Oswald" }}
+    >
+      <div className='px-2'>
+        <div className='flex items-center justify-center gap-2'>
+          <Store color='#208572' size={32} strokeWidth={2} />
+          <h1 className="text-center text-[#208572] font-bold text-2xl py-5">
+            Mi Tienda Online
+          </h1>
         </div>
-        <div className='bg-[#29386F] grid place-items-center rounded-xl cursor-pointer px-4'>
-          <SlidersHorizontal className='w-7 h-7' />
-        </div>
-      </section>
-      <hr className='text-[#52514F50] my-5' />
-      {/* SECCION DE PRODUCTS */}
-      <section className='grid grid-cols-2 gap-5 mx-2'>
-        <div>
-          <div className='relative'>
-            <img
-              className='rounded-[10px] object-cover border border-[#947D0D]'
-              src="/images/test_img1.jpg"
-              alt="imagen_1"
+        <section className='flex gap-4'>
+          <div className='relative flex-1'>
+            <Search color="#208572" size={21} strokeWidth={2.5} className='absolute top-2 left-3' />
+            <input
+              type="search"
+              placeholder="Buscar"
+              style={{ fontFamily: "Inter" }}
+              className="w-full rounded-xl bg-transparent px-5 pl-12 py-2 text-[#52514F] border border-[#208572] placeholder:text-[#52514F] text-sm placeholder:text-sm placeholder:font-normal outline-none"
             />
-            <Heart strokeWidth={2} color="#A22E30" className='cursor-pointer absolute top-2 right-2' />
           </div>
-          <div className='mt-2 flex items-start justify-between'>
-            <div className='text-[#1E1E1E]'>
-              <p className='font-medium text-sm'>Tarta de Chocolate</p>
-              <p className='text-lg font-bold leading-5'>S/10.00</p>
-            </div>
-            <div className='bg-[#29386F] grid place-items-center rounded-lg cursor-pointer'>
-              <Plus className='w-9 h-9' />
-            </div>
+          <div className='bg-[#208572] grid place-items-center rounded-xl cursor-pointer px-3.5'>
+            <SlidersHorizontal size={24} />
           </div>
-        </div>
-        <div>
-          <div className='relative'>
+        </section>
+        <section className='flex flex-col gap-4 mt-5'>
+          <div className='relative flex justify-between p-2.5 outline outline-[#208572] rounded-[10px]'>
             <img
-              className=' rounded-[10px] object-cover border border-[#947D0D]'
-              src="/images/test_img2.jpg"
-              alt="imagen_1"
+              src='/images/test_img1.jpg'
+              className='w-1/3 object-contain'
+              alt='Gaseosa Coca-Cola de 3L'
             />
-            <Heart strokeWidth={2} color="#A22E30" className='cursor-pointer absolute top-2 right-2' />
-          </div>
-          <div className='mt-2 flex items-start justify-between'>
-            <div className='text-[#1E1E1E]'>
-              <p className='font-medium text-sm'>Table Android 10''</p>
-              <p className='text-lg font-bold leading-5'>S/499.00</p>
+            <div className='flex flex-col justify-between w-2/3 pl-2'>
+              <div className='flex justify-between gap-2.5'>
+                <p className='text-sm text-[#292929]'>Gaseosa Coca-Cola de 3L</p>
+                <Heart color='#208572' size={22} className='flex-shrink-0' />
+              </div>
+              <div className='flex flex-col gap-2.5'>
+                <p className='text-[#208572] text-2xl font-semibold tracking-[-1px]'>S/ 9.00</p>
+                <button className='bg-[#208572] py-[5px] px-2.5 text-white rounded-[5px]'>AGREGAR</button>
+              </div>
             </div>
-            <div className='bg-[#29386F] grid place-items-center rounded-lg cursor-pointer'>
-              <Plus className='w-9 h-9' />
-            </div>
           </div>
-        </div>
-        <div>
-          <div className='relative'>
+          <div className='relative flex justify-between p-2.5 outline outline-[#208572] rounded-[10px]'>
             <img
-              className=' rounded-[10px] object-cover border border-[#947D0D]'
-              src="/images/test_img3.webp"
-              alt="imagen_1"
+              src='/images/test_img1.jpg'
+              className='w-1/3 object-contain'
+              alt='Gaseosa Coca-Cola de 3L'
             />
-            <Heart strokeWidth={2} color="#A22E30" className='cursor-pointer absolute top-2 right-2' />
-          </div>
-          <div className='mt-2 flex items-start justify-between'>
-            <div className='text-[#1E1E1E]'>
-              <p className='font-medium text-sm'>Mini impresora termica</p>
-              <p className='text-lg font-bold leading-5'>S/129.00</p>
+            <div className='flex flex-col justify-between w-2/3 pl-2'>
+              <div className='flex justify-between gap-2.5'>
+                <p className='text-sm text-[#292929]'>Gaseosa Coca-Cola de 3L</p>
+                <Heart color='#208572' size={22} className='flex-shrink-0' />
+              </div>
+              <div className='flex flex-col gap-2.5'>
+                <p className='text-[#208572] text-2xl font-semibold tracking-[-1px]'>S/ 9.00</p>
+                <button className='bg-[#208572] py-[5px] px-2.5 text-white rounded-[5px]'>AGREGAR</button>
+              </div>
             </div>
-            <div className='bg-[#29386F] grid place-items-center rounded-lg cursor-pointer'>
-              <Plus className='w-9 h-9' />
-            </div>
           </div>
-        </div>
-        <div>
-          <div className='relative'>
+          <div className='relative flex justify-between p-2.5 outline outline-[#208572] rounded-[10px]'>
             <img
-              className=' rounded-[10px] object-cover border border-[#947D0D]'
-              src="/images/test_img4.webp"
-              alt="imagen_1"
+              src='/images/test_img1.jpg'
+              className='w-1/3 object-contain'
+              alt='Gaseosa Coca-Cola de 3L'
             />
-            <Heart strokeWidth={2} color="#A22E30" className='cursor-pointer absolute top-2 right-2' />
-          </div>
-          <div className='mt-2 flex items-start justify-between gap-2'>
-            <div className='text-[#1E1E1E]'>
-              <p className='font-medium text-sm'>Tabla de picar de bambu</p>
-              <p className='text-lg font-bold leading-5'>S/18.50</p>
+            <div className='flex flex-col justify-between w-2/3 pl-2 gap-2'>
+              <div className='flex justify-between gap-2.5'>
+                <p className='text-sm text-[#292929]'>Gaseosa Coca-Cola de 3L</p>
+                <Heart color='#208572' size={22} className='flex-shrink-0' />
+              </div>
+              <div className='flex flex-col gap-2.5'>
+                <p className='text-[#208572] text-2xl font-semibold tracking-[-1px]'>S/ 9.00</p>
+                <button className='bg-[#208572] py-[5px] px-2.5 text-white rounded-[5px]'>AGREGAR</button>
+              </div>
             </div>
-            <div className='bg-[#29386F] grid place-items-center rounded-lg cursor-pointer'>
-              <Plus className='w-9 h-9' />
-            </div>
           </div>
-        </div>
-      </section>
-      {/* PAGINADOR */}
-      <section className='flex justify-center gap-3 my-8 mb-12'>
-        <div className='text-lg flex gap-1 justify-center font-semibold'>
-          <span className='cursor-pointer bg-[#292929] rounded-full px-2.5'>1</span>
-          <span className='cursor-pointer text-[#292929] rounded-full px-2'>2</span>
-          <span className='cursor-pointer text-[#292929] rounded-full px-2'>3</span>
-          <span className='cursor-pointer text-[#292929] rounded-full px-2'>4</span>
-        </div>
-        <div className='flex items-center text-[#292929] cursor-pointer'>
-          <p className='font-medium'>Siguiente</p>
-          <ChevronRight size={20} className=''/>
-        </div>
-      </section>
+        </section>
+        <section className='flex justify-center gap-3 my-8 mb-28' style={{ fontFamily: "Oswald" }}>
+          <div className='text-sm flex gap-1 items-center justify-center font-semibold'>
+            <span className='cursor-pointer bg-[#208572] rounded-full px-2.5 py-[2px] grid place-items-center'>1</span>
+            <span className='cursor-pointer text-[#292929] rounded-full px-2'>2</span>
+            <span className='cursor-pointer text-[#292929] rounded-full px-2'>3</span>
+            <span className='cursor-pointer text-[#292929] rounded-full px-2'>4</span>
+          </div>
+          <div className='bg-[#29292950] p-1.5 rounded-full text-sm flex justify-center items-center text-[#292929] cursor-pointer'>
+            <ChevronRight size={22} strokeWidth={3} />
+          </div>
+        </section>
+      </div>
       <Navbar />
     </section>
   )
